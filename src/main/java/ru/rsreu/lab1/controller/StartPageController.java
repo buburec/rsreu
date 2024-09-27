84b66b80a89e54c7078fecfbb2d29aecccfc9289
g143_popov.k.i <crasher402@gmail.com>
2024-09-27 14:10:01 +0300

    Лабораторная работа N1

---
+++ src/main/java/ru/rsreu/lab1/controller/StartPageController.java
@@ -0,0 +1,14 @@
+package ru.rsreu.lab1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartPageController {
+    @GetMapping("/home")
+    public String getHome(Model model) {
+        model.addAttribute("name", "магазин сантехники");
+        return "home";
+    }
+}
---
+++ src/test/java/ru/rsreu/lab1/Lab1ApplicationTests.java
@@ -0,0 +1,47 @@
+package ru.rsreu.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class Lab1ApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getHome() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void getLostPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    public void getNamePage() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(view().name("home"));
+    }
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(model().attribute("name", "магазин сантехники"));
+    }
+
+
+}
